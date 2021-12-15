a02f7f415a59c77567d993999063ff726592d8fe
Michael Matena <michael.matena@gmail.com>
2021-12-15 11:14:47 -0500

    Added first pass of Fisher stuff

--- src/wise_ft.py
+++ src/wise_ft.py
@@ -7,8 +7,38 @@ import torch
 from src.models.eval import evaluate
 from src.models.finetune import finetune
 from src.models.modeling import ClassificationHead, ImageEncoder, ImageClassifier
+from src.models.utils import fisher_load
 from src.models.zeroshot import get_zeroshot_classifier
 from src.args import parse_arguments
+
+
+def _merge(alpha, theta_0, theta_1, fishers, fisher_floor):
+    if fishers is None:
+        # interpolate between all weights in the checkpoints
+        return {
+            key: (1 - alpha) * theta_0[key] + alpha * theta_1[key]
+            for key in theta_0.keys()
+        }
+
+    fisher_0, fisher_1 = fishers
+
+    theta = {}
+    for key in theta_0.keys():
+        # Make sure that either we have a Fisher for this variable for
+        # both checkpoints or none of the checkpoints. Default to regular
+        # interpolation if no Fisher is found.
+        assert (key in fisher_0) == (key in fisher_1)
+        ones = torch.ones_like(theta_0[key])
+        f_0 = torch.maximum(fisher_0.get(key, ones), fisher_floor * ones)
+        f_1 = torch.maximum(fisher_1.get(key, ones), fisher_floor * ones)
+
+        c_0 = (1 - alpha) * f_0
+        c_1 = alpha * f_1
+
+        theta[key] = (c_0 * theta_0[key] + c_1 * theta_1[key]) / (c_0 + c_1)
+
+    return theta
+
 
 def wise_ft(args):
     assert args.save is not None, 'Please provide a path to store models'
@@ -38,6 +68,14 @@ def wise_ft(args):
     theta_1 = {k: v.clone() for k, v in finetuned.state_dict().items()}
     del zeroshot
 
+    if args.fisher is None:
+        fishers = None
+    else:
+        fisher_0_file, fisher_1_file = args.fisher
+        fisher_0 = fisher_load(os.path.expanduser(fisher_0_file))
+        fisher_1 = fisher_load(os.path.expanduser(fisher_1_file))
+        fishers = fisher_0, fisher_1
+
     # make sure checkpoints are compatible
     assert set(theta_0.keys()) == set(theta_1.keys())
 
@@ -45,11 +83,7 @@ def wise_ft(args):
     for alpha in alphas:
         args.alpha = alpha
 
-        # interpolate between all weights in the checkpoints
-        theta = {
-            key: (1-alpha) * theta_0[key] + alpha * theta_1[key]
-            for key in theta_0.keys()
-        }
+        theta = _merge(alpha, theta_0, theta_1, fishers, args.fisher_floor)
 
         # update the model (in-place) acccording to the new weights
         finetuned.load_state_dict(theta)
@@ -64,6 +98,3 @@ def wise_ft(args):
 if __name__ == '__main__':
     args = parse_arguments()
     wise_ft(args)
-
-
-
